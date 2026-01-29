79d4f9502ffc614f53a1c55869eb7ca4d0762ecc
leiweijie0114 <158798451+leiweijie0114@users.noreply.github.com>
2026-01-29 20:46:36 +0800

    更新

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from langchain.memory import ConversationBufferMemory
 
 from utils import get_chat_response
 
-st.title("💬 克隆Deepseek")
+st.title("💬 克隆ChatGPT")
 
 with st.sidebar:
-    openai_api_key = st.text_input("请输入Deepseek API Key：", type="password")
-    st.markdown("[获取Deepseek API key](https://platform.deepseek.com/api_keys)")
+    openai_api_key = st.text_input("请输入OpenAI API Key：", type="password")
+    st.markdown("[获取OpenAI API key](https://platform.openai.com/account/api-keys)")
 
 if "memory" not in st.session_state:
     st.session_state["memory"] = ConversationBufferMemory(return_messages=True)
